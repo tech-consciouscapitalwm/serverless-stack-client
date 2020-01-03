5e0bcfe3c58c160b4715832abb61b8a06bdca2ee
Curt Downing <tech@consciouscapitalwm.com>
2020-01-03 07:04:58 -0500

    First Commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://sbptocki2f.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_3aHGjgvCa",
+    APP_CLIENT_ID: "33e11t3sspla4pv8q7363ubon0",
+    IDENTITY_POOL_ID: "us-east-1:bb8d9484-2d9a-429f-ad80-1516d7695c82"
+  }
+};
